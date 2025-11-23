bef2d392d70969b9f95c0b9674178f5a50acbd14
Mikhail Demidov <miki933@mail.ru>
2025-11-23 19:46:30 +0400

    add cart

--- main/models.py
+++ main/models.py
@@ -26,7 +26,7 @@ class Size(models.Model):
     
 
 class ProductSize(models.Model):
-    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='product_size')
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='product_sizes')
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
     stock = models.PositiveBigIntegerField(default=0)
 
